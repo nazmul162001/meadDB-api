1deafe32bdcdb4639cb0a9f205cfbb83fa69bca7
nazmul162001 <nazmulprime162001@gmail.com>
2022-02-23 16:53:20 +0600

    update dynamic search & single food details include showing result in youtube

--- js/mealDB.js
+++ js/mealDB.js
@@ -18,11 +18,11 @@ const searchFood = () => {
 const displaySearchResult = meals => {
   const searchResult = document.getElementById('search-result');
   meals.forEach(meal => {
-    console.log(meal);
+    // console.log(meal);
     const div = document.createElement('div');
     div.classList.add('col');
     div.innerHTML = `
-    <div class="card">
+    <div onclick="loadMealDetail(${meal.idMeal})" class="card">
         <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">${meal.strMeal} </h5>
@@ -32,4 +32,32 @@ const displaySearchResult = meals => {
     `;
     searchResult.appendChild(div);
   })
+}
+
+// get dynamic details
+
+const loadMealDetail = mealId => {
+  console.log(mealId);
+  const url = `
+  https://www.themealdb.com/api/json/v1/1/lookup.php?i= ${mealId}
+  `;
+  fetch(url)
+  .then(res => res.json())
+  .then(data => displayMealDetail(data.meals[0]));
+}
+
+const displayMealDetail = meal => {
+  console.log(meal);
+  const mealDetails = document.getElementById('meal-details')
+  const div = document.createElement('div')
+  div.classList.add('card')
+  div.innerHTML = `
+  <img src="${meal.strMealThumb} " class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${meal.strMeal} </h5>
+    <p class="card-text">${meal.strInstructions.slice(0, 150)}</p>
+    <a href="${meal.strYoutube}" class="btn btn-primary">Go somewhere</a>
+  </div>
+  `;
+  mealDetails.appendChild(div);
 }
