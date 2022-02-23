d6d3ccdbc378b92dcfaeb323cf1091b3b0ab090d
nazmul162001 <nazmulprime162001@gmail.com>
2022-02-23 16:17:49 +0600

    add search food api & custome style

---
+++ js/mealDB.js
@@ -0,0 +1,35 @@
+const searchFood = () => {
+  const searchField = document.getElementById('search-field');
+  const searchText = searchField.value;
+  // console.log(searchText);
+
+  const url = `
+  https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}
+  `;
+  // console.log(url);
+  fetch (url)
+  .then(res => res.json())
+  .then(data => displaySearchResult(data.meals));
+
+  searchField.value = '';
+}
+
+
+const displaySearchResult = meals => {
+  const searchResult = document.getElementById('search-result');
+  meals.forEach(meal => {
+    console.log(meal);
+    const div = document.createElement('div');
+    div.classList.add('col');
+    div.innerHTML = `
+    <div class="card">
+        <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${meal.strMeal} </h5>
+          <p class="card-text">${meal.strInstructions.slice(0, 150)} </p>
+        </div>
+    </div>
+    `;
+    searchResult.appendChild(div);
+  })
+}
